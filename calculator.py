5f75d7666925b87f8a80e6a60759282f85831785
anilademyener <anilademyener@gmail.com>
2019-08-07 14:07:24 +0000

    added install and uninstall option

--- calculator.py
+++ calculator.py
@@ -2,6 +2,24 @@
 import os
 import sys
 
+folderName = "calculator-gui"
+command = ('#!/usr/bin/env python3\n'
+					  'import os\n'
+					  'if __name__ == "__main__":\n'
+					  '		os.system("nw /usr/bin/'+folderName+'")')
+
+def install():
+	os.system("sudo mkdir /usr/bin/"+folderName)
+	os.system("sudo cp -r * /usr/bin/"+folderName)
+	os.system("sudo touch tempInstall.py")
+	os.system("sudo chmod 777 tempInstall.py")
+	os.system("echo "+"'"+command+"'"+" > tempInstall.py")
+	os.system("sudo mv tempInstall.py /usr/bin/"+folderName+".py")
+
+def uninstall():
+	os.system("sudo rm -rf /usr/bin/"+folderName)
+	os.system("sudo rm -rf /usr/bin/"+folderName+".py")
+
 if __name__ == "__main__":
 	if sys.platform == "linux":
 		if len(sys.argv) == 1:
@@ -10,6 +28,6 @@ if __name__ == "__main__":
 				os.system("nw .")
 		else:
 			if sys.argv[1] == "--install":
-				print("soon")
+				install()
 			elif sys.argv[1] == "--uninstall":
-				print("soon")
+				uninstall()
